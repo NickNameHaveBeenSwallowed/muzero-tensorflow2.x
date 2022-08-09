aca9d9ad0fde7489dc8cd39dd5d1d061370230bd
NickNameHaveBeenSwallowed <79394167+NickNameHaveBeenSwallowed@users.noreply.github.com>
2022-08-09 20:11:51 +0800

    Update trainer.py

--- gomoku/trainer.py
+++ gomoku/trainer.py
@@ -25,7 +25,8 @@ class ReplayBuffer():
         t1, t2, t3, t4, t5, t6, t7, t8 = [], [], [], [], [], [], [], []
         for s_a_p_w in trajectory:
             state, action, policy, winner = s_a_p_w
-            policy = np.reshape(policy, newshape=(action.shape[0], action.shape[1]))
+            policy = np.reshape(policy, newshape=(action.shape[0], action.shape[1], 1))
+            action = np.reshape(action, newshape=(action.shape[0], action.shape[1], 1))
             state_flip_1, action_flip_1, policy_flip_1, winner_flip_1 = tf.image.flip_left_right(state), tf.image.flip_left_right(action), tf.image.flip_left_right(policy), winner
             state_rot90, action_rot90, policy_rot90, winner_rot90 = tf.image.rot90(state, k=1), tf.image.rot90(action, k=1), tf.image.rot90(policy, k=1), winner
             state_flip_2, action_flip_2, policy_flip_2, winner_flip_2 = tf.image.flip_left_right(state_rot90), tf.image.flip_left_right(action_rot90), tf.image.flip_left_right(policy_rot90), winner
@@ -33,14 +34,14 @@ class ReplayBuffer():
             state_flip_3, action_flip_3, policy_flip_3, winner_flip_3 = tf.image.flip_left_right(state_rot180), tf.image.flip_left_right(action_rot180), tf.image.flip_left_right(policy_rot180), winner
             state_rot270, action_rot270, policy_rot270, winner_rot270 = tf.image.rot90(state, k=3), tf.image.rot90(action, k=3), tf.image.rot90(policy, k=3), winner
             state_flip_4, action_flip_4, policy_flip_4, winner_flip_4 = tf.image.flip_left_right(state_rot270), tf.image.flip_left_right(action_rot270), tf.image.flip_left_right(policy_rot270), winner
-            t1.append([state, action, np.reshape(policy, newshape=(policy.shape[0] * policy.shape[1])), winner])
-            t2.append([state_flip_1, action_flip_1, np.reshape(policy_flip_1, newshape=(policy_flip_1.shape[0] * policy_flip_1.shape[1])), winner_flip_1])
-            t3.append([state_rot90, action_rot90, np.reshape(policy_rot90, newshape=(policy_rot90.shape[0] * policy_rot90.shape[1])), winner_rot90])
-            t4.append([state_flip_2, action_flip_2, np.reshape(policy_flip_2, newshape=(policy_flip_2.shape[0] * policy_flip_2.shape[1])), winner_flip_2])
-            t5.append([state_rot180, action_rot180, np.reshape(policy_rot180, newshape=(policy_rot180.shape[0] * policy_rot180.shape[1])), winner_rot180])
-            t6.append([state_flip_3, action_flip_3, np.reshape(policy_flip_3, newshape=(policy_flip_3.shape[0] * policy_flip_3.shape[1])), winner_flip_3])
-            t7.append([state_rot270, action_rot270, np.reshape(policy_rot270, newshape=(policy_rot270.shape[0] * policy_rot270.shape[1])), winner_rot270])
-            t8.append([state_flip_4, action_flip_4, np.reshape(policy_flip_4, newshape=(policy_flip_4.shape[0] * policy_flip_4.shape[1])), winner_flip_4])
+            t1.append([state, np.reshape(action, newshape=(action.shape[0], action.shape[1])), np.reshape(policy, newshape=(policy.shape[0] * policy.shape[1])), winner])
+            t2.append([state_flip_1, np.reshape(action_flip_1, newshape=(action_flip_1.shape[0], action_flip_1.shape[1])), np.reshape(policy_flip_1, newshape=(policy_flip_1.shape[0] * policy_flip_1.shape[1])), winner_flip_1])
+            t3.append([state_rot90, np.reshape(action_rot90, newshape=(action_rot90.shape[0], action_rot90.shape[1])), np.reshape(policy_rot90, newshape=(policy_rot90.shape[0] * policy_rot90.shape[1])), winner_rot90])
+            t4.append([state_flip_2, np.reshape(action_flip_2, newshape=(action_flip_2.shape[0], action_flip_2.shape[1])), np.reshape(policy_flip_2, newshape=(policy_flip_2.shape[0] * policy_flip_2.shape[1])), winner_flip_2])
+            t5.append([state_rot180, np.reshape(action_rot180, newshape=(action_rot180.shape[0], action_rot180.shape[1])), np.reshape(policy_rot180, newshape=(policy_rot180.shape[0] * policy_rot180.shape[1])), winner_rot180])
+            t6.append([state_flip_3, np.reshape(action_flip_3, newshape=(action_flip_3.shape[0], action_flip_3.shape[1])), np.reshape(policy_flip_3, newshape=(policy_flip_3.shape[0] * policy_flip_3.shape[1])), winner_flip_3])
+            t7.append([state_rot270, np.reshape(action_rot270, newshape=(action_rot270.shape[0], action_rot270.shape[1])), np.reshape(policy_rot270, newshape=(policy_rot270.shape[0] * policy_rot270.shape[1])), winner_rot270])
+            t8.append([state_flip_4, np.reshape(action_flip_4, newshape=(action_flip_4.shape[0], action_flip_4.shape[1])), np.reshape(policy_flip_4, newshape=(policy_flip_4.shape[0] * policy_flip_4.shape[1])), winner_flip_4])
         return t1, t2, t3, t4, t5, t6, t7, t8
 
 
